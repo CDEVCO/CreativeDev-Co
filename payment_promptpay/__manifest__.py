0fc1a1ef40c6c45b4edf6cb2f342620cc4e96718
Soe Htet Linn <mgsoehtetlinn@gmail.com>
2024-09-18 13:07:05 +0700

    Add payment_promptpay module

---
+++ payment_promptpay/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'payment_promptpay',
+    'summary': 'Payment Provider module of Promptpay',
+    'description': 'Payment Provider module of Promptpay',
+    'author': 'Creativedev co',
+    'website': '',
+    'category': 'payment',
+    'version': '17.0',
+    'depends': ['base', 'payment'],
+    'data': [
+        'views/view_sample_model.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+}
