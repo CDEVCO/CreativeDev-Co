076d5abfd2633cb897f084b704b4e64b6f61d438
Soe Htet Linn <mgsoehtetlinn@gmail.com>
2024-09-26 14:16:49 +0700

    Add material consumption in recipe management

--- cd_recipe_material_management/__manifest__.py
+++ cd_recipe_material_management/__manifest__.py
@@ -7,11 +7,19 @@
     'website': '',
     'category': '',
     'version': '17.0',
-    'depends': ['base','sale', 'product', 'stock'],
+    'depends': ['base','sale', 'product', 'point_of_sale','stock'],
     'data': [
+        'data/sequence_data.xml',
+        'views/view_material_consumption.xml',
         'views/view_product.xml',
+        'views/view_point_of_sale.xml',
         'security/ir.model.access.csv',
     ],
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'cd_recipe_material_management/static/src/js/cart.js',
+        ]
+    },
     'demo': [],
     'installable': True,
     'application': True,
