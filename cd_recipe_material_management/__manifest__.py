215cbd04e895c00589615f609f328bde12ce669e
Alex <dynaz@live.com>
2024-09-26 14:39:40 +0700

    description for Recipe

--- cd_recipe_material_management/__manifest__.py
+++ cd_recipe_material_management/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'cd_recipe_material_management',
+    'name': 'Recipe and Material Consumption Management',
     'summary': 'Recipe and Material Consumption Management',
     'description': 'Recipe and Material Consumption Management',
-    'author': 'creativedev co',
-    'website': '',
-    'category': '',
+    'author': 'Creative Dev Co.,Ltd.',
+    'website': 'https://creativedev.co.th/en',
+    'category': 'Inventory',
     'version': '17.0',
     'depends': ['base','sale', 'product', 'stock'],
     'data': [
@@ -15,4 +15,7 @@
     'demo': [],
     'installable': True,
     'application': True,
+    'price': '65.49',
+    'currency': 'USD',
+    "images": ["static/description/banner.png"],
 }
